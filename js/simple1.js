7afee71fd79ce16ab8917744e77907c3cdc2bd52
kaseopea <ehjlsehjls>
2016-06-15 16:45:13 +0300

    random changes

--- js/simple1.js
+++ js/simple1.js
@@ -1,39 +1,39 @@
-// -----------------------------------------
-// Task 1 Треугольник в цикле
-// -----------------------------------------
-var treeSize = 7;
-var line = '';
-
-//for (var line = "#"; line.length <= treeSize; line += "#") console.log(line);
-
-// -----------------------------------------
-// Task 2 FizzBuzz
-// -----------------------------------------
-var startNumber = 1;
-var endNumber = 100;
-var i, out;
-for (i = startNumber; i<=endNumber; i++) {
-    var out = '';
-    
-    if (i%3 == 0 ) out += 'Fizz';
-    if (i%5 == 0) out += 'Buzz';
-    //console.log(out || i);
-}
-
-// -----------------------------------------
-// Task 3
-// -----------------------------------------
-var size_rows = 8;
-var size_columns = 16;
-var board = '';
-
-for (var y = 0; y < size_rows; y++ ) {
-    for (var x = 0; x < size_columns; x++ ) {
-        if ((x+y)%2 == 0)
-            board += ' ';
-        else
-            board += '*';
+////////////////////////////////////////////////////////////////////////////////////////////////////
+function isPrime(num) {
+  var isSimple = 0;
+  if (num > 1) {
+    for ( var i = 2; i <= num; i++ ) {
+      if((num%i) == 0) isSimple++;
+      if (isSimple == 2) break;
     }
-    board += '\n';
+    if (isSimple < 2) return true;
+  } return false;
 }
-console.log(board);
+
+// console.log(isPrime(0));
+// console.log(isPrime(1));
+// console.log(isPrime(2));
+// console.log(isPrime(151));
+// console.log(isPrime(2560));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+var arr = [1,3,4,4,4,4,5];
+var example = [4,1,2,3,5];
+//Result: [4,4,4,4,1,3,5]
+
+
+var sortByExample = function (arr, example) {
+  var result = [];
+  for ( var i = 0; i < example.length; i++) {
+    var search = arr.filter(function(value){
+      return value == example[i];
+    });
+    result = result.concat(arr.filter(function(value){
+      return value == example[i];
+    }));
+  }
+  return result;
+}
+
+console.log(sortByExample(arr,example));
